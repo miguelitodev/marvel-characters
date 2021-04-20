279f491bb4969855ffe6450b880a98cda28ca7f4
miguelrisquelme <miguelrisquelme@gmail.com>
2021-04-20 18:45:29 -0300

    adding character page

--- src/pages/Character/index.js
+++ src/pages/Character/index.js
@@ -1,9 +1,115 @@
-import "./styles";
+import {
+    Header,
+    Title,
+    GoBack,
+    Main,
+    MainContent,
+    ImageCharacter,
+    ContentCharacter,
+    NameCharacter,
+    DescriptionCharacter,
+    ListComics,
+    TitleStage,
+    ContentComics,
+    Comics,
+    ImageComics,
+    TextComics,
+    TitleComics,
+    DescriptionComics,
+} from "./styles";
+
+import Container from "./../../components/Container";
 
 export default function Character() {
     return (
-        <>
-            <h1>Character</h1>
-        </>
+        <Container>
+            <Header>
+                <Title>Abomination (Emil Blonsky)</Title>
+                <GoBack>voltar</GoBack>
+            </Header>
+            <Main>
+                <MainContent>
+                    <ImageCharacter
+                        src="http://i.annihil.us/u/prod/marvel/i/mg/9/50/4ce18691cbf04.jpg"
+                        alt="personagem"
+                    />
+                    <ContentCharacter>
+                        <NameCharacter>
+                            Abomination (Emil Blonsky)
+                        </NameCharacter>
+                        <DescriptionCharacter>
+                            Formerly known as Emil Blonsky, a spy of Soviet
+                            Yugoslavian origin working for the KGB, the
+                            Abomination gained his powers after receiving a dose
+                            of gamma radiation similar to that which transformed
+                            Bruce Banner into the incredible Hulk.
+                        </DescriptionCharacter>
+                    </ContentCharacter>
+                </MainContent>
+                <ListComics>
+                    <TitleStage>Fascículos</TitleStage>
+                    <ContentComics>
+                        <Comics>
+                            <ImageComics
+                                src="http://i.annihil.us/u/prod/marvel/i/mg/2/e0/586bcdf9c3ecd.jpg"
+                                alt="comic"
+                            />
+                            <TextComics>
+                                <TitleComics>Avengers (1998) #61</TitleComics>
+                                <DescriptionComics>
+                                    A temporada de classificação de 2021 está
+                                    finalmente aqui depois de alguns meses
+                                    brincando com os itens novos e brilhantes da
+                                    pré-temporada. Quer você tenha ficado
+                                    desapontado com a classificação da última
+                                    temporada, queira subir mais alto do que
+                                    nunca, ou se você for completamente novo no
+                                    jogo, o grind de classificação começou.
+                                    Então, se você quiser aumentar seu pool de
+                                    campeões para ter certeza de que está
+                                    pronto, aqui estão nossas melhores escolhas
+                                    para ajudá-lo a escalar em cada função!
+                                    jogo, o grind de classificação começou.
+                                    Então, se você quiser aumentar seu pool de
+                                    campeões para ter certeza de que está
+                                    pronto, aqui estão nossas melhores escolhas
+                                    para ajudá-lo a escalar em cada função!
+                                </DescriptionComics>
+                            </TextComics>
+                        </Comics>
+                        <Comics>
+                            <ImageComics
+                                src="http://i.annihil.us/u/prod/marvel/i/mg/2/e0/586bcdf9c3ecd.jpg"
+                                alt="comic"
+                            />
+                            <TextComics>
+                                <TitleComics>Avengers (1998) #61</TitleComics>
+                                <DescriptionComics>
+                                    The team gains new international status, but
+                                    will everyone accept it? Avengers come and
+                                    go as the new lineup is established and new
+                                    threats await!
+                                </DescriptionComics>
+                            </TextComics>
+                        </Comics>
+                        <Comics>
+                            <ImageComics
+                                src="http://i.annihil.us/u/prod/marvel/i/mg/2/e0/586bcdf9c3ecd.jpg"
+                                alt="comic"
+                            />
+                            <TextComics>
+                                <TitleComics>Avengers (1998) #61</TitleComics>
+                                <DescriptionComics>
+                                    The team gains new international status, but
+                                    will everyone accept it? Avengers come and
+                                    go as the new lineup is established and new
+                                    threats await!
+                                </DescriptionComics>
+                            </TextComics>
+                        </Comics>
+                    </ContentComics>
+                </ListComics>
+            </Main>
+        </Container>
     );
 }
--- src/pages/Character/styles.js
+++ src/pages/Character/styles.js
@@ -1,1 +1,153 @@
 import styled from "styled-components";
+
+export const Header = styled.header`
+    width: 100%;
+    padding: 50px 0px;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    @media (max-width: 1024px) {
+        padding: 50px 15px;
+    } ;
+`;
+
+export const Title = styled.h2`
+    color: var(--title);
+    text-transform: uppercase;
+`;
+
+export const GoBack = styled.button`
+    width: 100px;
+    height: 50px;
+    padding: 5px;
+
+    text-transform: uppercase;
+    color: var(--white);
+    font-weight: 800;
+
+    border: none;
+    border-radius: 5px;
+    background-color: var(--green-accepted);
+
+    &:focus {
+        outline: dotted 1px var(--border);
+    }
+`;
+
+export const Main = styled.main`
+    width: 80%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    padding-bottom: 50px;
+`;
+export const MainContent = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    padding: 50px 0px;
+
+    @media (max-width: 1024px) {
+        flex-direction: column;
+    }
+`;
+
+export const ImageCharacter = styled.img`
+    width: 360px;
+    height: 360px;
+    object-fit: cover;
+
+    border-radius: 10px;
+    -webkit-box-shadow: 0px 0px 5px 0px var(--shadow);
+    -moz-box-shadow: 0px 0px 5px 0px var(--shadow);
+    box-shadow: 0px 0px 5px 0px var(--shadow);
+
+    @media (max-width: 1024px) {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+`;
+
+export const ContentCharacter = styled.div`
+    width: 50%;
+
+    @media (max-width: 1024px) {
+        width: 100%;
+        margin-top: 15px;
+    }
+`;
+
+export const NameCharacter = styled.h1`
+    color: var(--title);
+`;
+
+export const DescriptionCharacter = styled.p`
+    color: var(--text);
+    text-align: justify;
+`;
+
+export const ListComics = styled.div``;
+
+export const TitleStage = styled.h2`
+    text-align: center;
+    margin: 60px 0;
+    color: var(--title);
+    font-size: 30px;
+`;
+
+export const ContentComics = styled.div`
+    display: grid;
+    grid-template-columns: 45% 45%;
+    justify-content: space-between;
+
+    @media (max-width: 1024px) {
+        grid-template-columns: 100%;
+    }
+`;
+
+export const Comics = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    flex-direction: row;
+    width: 100%;
+    margin-top: 50px;
+`;
+
+export const ImageComics = styled.img`
+    width: 200px;
+    height: 311px;
+    object-fit: cover;
+`;
+
+export const TextComics = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    margin-left: 15px;
+`;
+
+export const TitleComics = styled.h3`
+    color: var(--title);
+    margin-bottom: 5px;
+`;
+
+export const DescriptionComics = styled.p`
+    display: block;
+    display: -webkit-box;
+    -webkit-line-clamp: 15;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    @media (max-width: 1024px) {
+        -webkit-line-clamp: 15;
+    }
+`;
